2084c22774a90390b00e90a6d96a567c2b7700b2
Vostaa <not.amellctrl@gmail.com>
2026-08-09 15:15:21 +0700

    if statement

--- IF_STATEMENT.cpp
+++ IF_STATEMENT.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+     int harga_makanan = 50000;
+     int uang_jajan;
+
+     cout << "harga makanannya adalah: " << harga_makanan << endl;
+     cout << "Berapa uang jajanmu? ";
+     cin >> uang_jajan;
+
+     if(uang_jajan >= harga_makanan) {
+        cout << "UANGMU CUKUP!" << endl;
+     }
+     else{
+        cout << "MOHON MAAF, UANGMU KURANG.....";
+     }
+
     return 0;
 }
